ed5ce5a37e27072124e16ef30b823ff3d65f0957
Digvijay <sompuradigvijay999@gmail.com>
2024-01-03 08:05:48 +0530

    new button task code created for event learning

--- react1/src/component/Button.js
+++ react1/src/component/Button.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Button({children,onPlay,onPause}) {
   let playing = true;
-  const handleclick =()=>{
+  const handleclick =(e)=>{
+    // console.log(e.ctrlKey);
+    e.stopPropagation();
+    // console.log(e);
     if(playing) onPause()
     else onPlay()
     
