ae03688227b8ac48cb3000be5d730cba90e72a47
Digvijay <sompuradigvijay999@gmail.com>
2024-01-04 05:46:33 +0530

    call task1 componenet

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,48 +1,11 @@
-
 import './App.css';
-import Button from './component/Button';
-
-import Video from './component/Video';
+import Task1 from './component/Task1';
 
 function App() {
-  let videos=[{
-          id:1,
-          title:"Dog",
-          view:"5K",
-          time:"1 year",
-          varify: true
-        },
-        {
-          id:2,
-          title:"Cat",
-          view:"1K",
-          time:"1 month",
-          varify: true
-        },
-        {
-          id:3,
-          title:"Rat",
-          view:"10K",
-          time:"5 year",
-          varify: false
-        }
-      ]
   return (
     <>
-    <div className="container" onClick={()=>console.log('app')}>
-      {/* <Button btnlabel="Play button" onPlay={()=> console.log('play')} onPause={()=> console.log('pause')}>Play </Button> */}
-      {/* <Button btnlabel="pause button" onSmash={()=> console.log('pause')}>Pause</Button> */}
-      {
-        videos.map(video=>
-          <Video title={video.title}
-          view={video.view}
-          time={video.time}
-          varify= {video.varify}
-          id={video.id} key={video.id}>
-            <Button onPlay={()=> console.log('playing..')} onPause={()=> console.log('paused..')}>{video.title}</Button>  
-          </Video>
-          )
-      }
+    <div className="container">
+      <Task1 ></Task1>
     </div>
     </>
   );
