57272fc0bed106c72a13f881cea66f50f67d21c4
Digvijay <sompuradigvijay999@gmail.com>
2024-01-10 08:37:58 +0530

    digital clock task is done

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import Button from './component/Button';
+import Clocktask from './component/Clocktask';
+// import Button from './component/Button';
 // import Comp2 from './component/Comp2';
 // import Resume from './component/Resume';
 // import Task1 from './component/Task1';
-import Video from './component/Video';
+// import Video from './component/Video';
 // import Counter from './component/Counter';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 function App() {
-  console.log("app render");
+  // console.log("app render");
   // let resumeObj = {
   //   mainTitle: "Resume",
   //   userName: "Emily",
@@ -32,49 +33,49 @@ function App() {
   //   // alert("test")
   // }
 
-  let videos=[{
-          title:"Dog",
-          view:"5K",
-          time:"1 year",
-          varify: true,
-          id: 1
-        },
-        {
-          title:"Cat",
-          view:"1K",
-          time:"1 month",
-          varify: true,
-          id: 2
-        },
-        {
-          title:"Rat",
-          view:"10K",
-          time:"5 year",
-          varify: false,
-          id: 3
-        }
-      ]
+  // let videos=[{
+  //         title:"Dog",
+  //         view:"5K",
+  //         time:"1 year",
+  //         varify: true,
+  //         id: 1
+  //       },
+  //       {
+  //         title:"Cat",
+  //         view:"1K",
+  //         time:"1 month",
+  //         varify: true,
+  //         id: 2
+  //       },
+  //       {
+  //         title:"Rat",
+  //         view:"10K",
+  //         time:"5 year",
+  //         varify: false,
+  //         id: 3
+  //       }
+  //     ]
     
-    const [videoData, setVideoData] = useState(videos);
+  //   const [videoData, setVideoData] = useState(videos);
 
-    let  handleVideo = ()=>{
-      console.log(videos.length);
-      setVideoData([...videoData,{
-        title:"Rat",
-        view:"10K",
-        time:"5 year",
-        varify: false,
-        id: videoData.length+1
-      }]) 
-  }
+  //   let  handleVideo = ()=>{
+  //     console.log(videos.length);
+  //     setVideoData([...videoData,{
+  //       title:"Rat",
+  //       view:"10K",
+  //       time:"5 year",
+  //       varify: false,
+  //       id: videoData.length+1
+  //     }]) 
+  // }
   return (
     <>
-    <button onClick={handleVideo}>Add video</button> 
+    {/* <button onClick={handleVideo}>Add video</button>  */}
     <div className="container">
       {/* <Task1 ></Task1> */}
       {/* <Resume {...resumeObj}></Resume> */}
       {/* <Comp2 event={clickTest}></Comp2> */}
-      {
+      {/* {
         videoData.map(video=>
         <Video title={video.title}
         view={video.view}
@@ -86,8 +87,10 @@ function App() {
         </Video>
         )
   
-      }
+      } */}
       {/* <Counter></Counter> */}
+
+      <Clocktask></Clocktask>
     </div>
     </>
   );
---
+++ react1/src/component/Clocktask.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './clocktask.css'
+import { useState, useEffect } from 'react'
+
+function Clocktask() {
+// const [time, setTime]=useState(new Date().toLocaleDateString());
+
+const [hour, setHour]=useState(new Date().getHours());
+const [minute, setMinute]=useState(new Date().getMinutes());
+const [second, setSecond]=useState(new Date().getSeconds());
+
+useEffect(()=>{
+    const interval = setTimeout(()=>{
+        // setTime(new Date().toLocaleTimeString());
+        setHour(new Date().getHours());
+        setMinute(new Date().getMinutes());
+        setSecond(new Date().getSeconds());
+        console.log(new Date().toLocaleTimeString());
+    });
+
+    return () => clearInterval(interval);
+});
+  return (
+    <div>
+        {/* <div className='timer'>
+            <h1>Digital Timer</h1>
+            <p>{time}</p>
+        </div> */}
+        <section>
+            <div class="clock">
+            <div class="bigtime">
+                <div class="time numbers hr"><span id="hours">{hour}</span></div>
+                <div class="time hr dot"id="blink"><span>:</span></div> 
+                <div class="time numbers hr"><span id="minutes">{minute}</span></div>
+                </div>
+                <div class="lowtime">
+                <div class="time bottom numbers"><span id="seconds">{second}</span></div>
+                <div class="time bottom low"><span id="sessions">{hour>12?'PM':'AM'}</span></div>
+            </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+
+export default Clocktask
